37b68f198910cfd3fba53cb9faf1d1692a03dbbe
artivus2-prog <artivus2@gmail.com>
2026-05-14 20:15:44 +0500

    При старте книжный режим, альбомный только в игре

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'screens/home_screen.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  // Принудительно альбомная ориентация
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.landscapeLeft,
-    DeviceOrientation.landscapeRight,
-  ]);
   runApp(const UnoApp());
 }
 
